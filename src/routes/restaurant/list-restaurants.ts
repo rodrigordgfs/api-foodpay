c5e6189e4cbb09132f24d72ad984754e3478ef28
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-23 15:16:25 -0300

    Feature: Ajustado a listagem de restaurantes

--- src/routes/restaurant/list-restaurants.ts
+++ src/routes/restaurant/list-restaurants.ts
@@ -21,6 +21,9 @@ export async function listRestaurants(app: FastifyInstance) {
               },
             },
           },
+          orderBy: {
+            updatedAt: "desc",
+          }
         });
 
         return reply.status(StatusCodes.OK).send(restaurants);
