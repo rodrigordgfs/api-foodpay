37d81c3b2ad7ab8856d2d4eb9d3b3cb83bc489ff
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-02-23 15:33:04 -0300

    Feature: Adicionado limit na listagem de restaurantes

--- src/routes/restaurant/list-restaurants.ts
+++ src/routes/restaurant/list-restaurants.ts
@@ -1,12 +1,25 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../../lib/prisma";
 import { StatusCodes } from "http-status-codes";
+import { z, ZodError } from "zod";
+
+interface ListRestaurantsRequestQuery {
+  limit?: number;
+}
+
+const listRestaurantsRequestQuerySchema = z.object({
+  limit: z.string().optional(),
+});
 
 export async function listRestaurants(app: FastifyInstance) {
   app.get(
     "/restaurant",
     async (request: FastifyRequest, reply: FastifyReply) => {
       try {
+        const { limit } = listRestaurantsRequestQuerySchema.parse(
+          request.query
+        ) as ListRestaurantsRequestQuery;
+
         const restaurants = await prisma.restaurant.findMany({
           include: {
             rates: {
@@ -23,7 +36,8 @@ export async function listRestaurants(app: FastifyInstance) {
           },
           orderBy: {
             updatedAt: "desc",
-          }
+          },
+          take: Number(limit) || undefined,
         });
 
         return reply.status(StatusCodes.OK).send(restaurants);
